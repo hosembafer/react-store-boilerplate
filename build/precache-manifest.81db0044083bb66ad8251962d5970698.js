331497fb777d5170e3d943c903ce4d1b7e6c559b
Rafael Hovhannisyan <hovhannisian.rafael@gmail.com>
2019-07-17 19:41:20 +0400

    Build

---
+++ build/precache-manifest.81db0044083bb66ad8251962d5970698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3b0228356748868a47963d3f1af40b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1b4d9dcb335e497da6",
+    "url": "/static/css/main.dbe1471b.chunk.css"
+  },
+  {
+    "revision": "497bdc9aa9fab9818b83",
+    "url": "/static/js/2.f08cb951.chunk.js"
+  },
+  {
+    "revision": "5d1b4d9dcb335e497da6",
+    "url": "/static/js/main.05e79656.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
