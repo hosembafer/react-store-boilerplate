66668806366c51fad9899dc3a084676700219ef9
Rafael Hovhannisyan <hovhannisian.rafael@gmail.com>
2019-07-17 20:03:47 +0400

    Updates

---
+++ precache-manifest.84fb6a534b4382708507384b93182b36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c248e7f4cc342e8b3c93a8f1918b6115",
+    "url": "/react-store-boilerplate/index.html"
+  },
+  {
+    "revision": "6aaa3d83ff84676b11ce",
+    "url": "/react-store-boilerplate/static/css/main.72461756.chunk.css"
+  },
+  {
+    "revision": "497bdc9aa9fab9818b83",
+    "url": "/react-store-boilerplate/static/js/2.f08cb951.chunk.js"
+  },
+  {
+    "revision": "6aaa3d83ff84676b11ce",
+    "url": "/react-store-boilerplate/static/js/main.0cc0641f.chunk.js"
+  },
+  {
+    "revision": "a72952103954d68eb940",
+    "url": "/react-store-boilerplate/static/js/runtime~main.beb96161.js"
+  }
+]);
