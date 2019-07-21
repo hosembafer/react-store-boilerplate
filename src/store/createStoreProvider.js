e45e448f6ce111abce52c132bc60d3066b4ce24b
Rafael Hovhannisyan <hovhannisian.rafael@gmail.com>
2019-07-21 11:41:44 +0400

    Fixed consuming order in createStoreProvider

--- src/store/createStoreProvider.js
+++ src/store/createStoreProvider.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default providers => ({ children }) => providers.reduce((tree, Provider) => <Provider>{tree}</Provider>, children);
+export default providers => ({ children }) => providers.reverse().reduce((tree, Provider) => <Provider>{tree}</Provider>, children);
